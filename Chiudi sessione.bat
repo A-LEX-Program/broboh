ecff8096c16816ae68ee582680055b9f9128f33d
A-LEX-Program <aledalte2015@gmail.com>
2025-03-19 21:12:49 +0100

    Add files via upload

---
+++ Chiudi sessione.bat
@@ -0,0 +1,3 @@
+@echo off
+echo alessandro spegni il computer e vai a prendere il latte
+pause
---
+++ accaso.bat
@@ -0,0 +1,33 @@
+@echo off
+echo una cosa a caso
+pause
+echo ho fatto na cosa a caso
+pause
+echo non so pk l'ho fatta
+pause
+echo forse perche' mi annoiavo
+pause
+echo fammi vedere le tue credenziali di rete
+pause
+echo %userprofile%
+echo questo e' il path del tuo profilo
+pause
+echo %username%
+echo questo e' il tuo nome utente
+pause
+ipconfig /all
+echo e questa e' la rete !
+pause
+echo fammi installare un virus :)))))))))))))))
+pause
+echo HAI FINITO DI GIOCARE CON IL TUO COMPUTER!
+pause
+echo CCCCCCCCCCCCCCRRRRRRRRRRRRRRRRAAAAAAAAAAAAAAAAAAAASSSSSSSSSSSSSSSSSHHHHHHHHHHHHHHHHHHHHH!
+pause
+echo sai una cosa? ti daro' un antivirus instantaneo per proteggerti
+pause
+echo in culo alla balena!
+pause
+echo @echo off>>antivirus.bat
+echo echo scherzetto ;)>>antivirus.bat
+echo pause>>antivirus.bat
